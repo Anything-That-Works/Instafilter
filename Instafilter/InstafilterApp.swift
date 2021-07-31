9ff642f623f9a0fce89d6377fcc6073a5f2596e7
Promal B <promal.barua.cse@gmail.com>
2021-08-01 02:59:07 +0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Promal on 1/8/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
